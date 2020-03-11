1579cc4561025734289ae92e8b01f9bdd54985a9
Eric Park <epark03@uoguelph.ca>
2020-03-11 16:51:02 -0400

    Adjusted 2nd table (error proofing), preparing for additional features

--- public/index.js
+++ public/index.js
@@ -33,13 +33,14 @@ $(document).ready(function() {
         dataType: 'json',
         url: '/loadTable',
         success: function(filelog) {
-            var filetable = "<table>";
+            console.log ("file log successfully loaded"); //Error message for console
+            var filetable = '<table class="center">';
             var fileHeader = ["Image (click to download)", "File name (click to download)", "File size", "Number of rectangles", "Number of circles", "Number of paths", "Number of groups"];
             //If returned array length is 0, display alt message
             if (filelog.SVGLog.length == 0) {
                 console.log("empty");
                 filetable += '<tr>';
-                filetable += '<td>' + 'No files';
+                filetable += '<td class="noFiles">' + 'No files';
                 filetable += '</td></tr>';
                 
             } else { //Otherwise, run for loop that creates the table,
@@ -71,6 +72,7 @@ $(document).ready(function() {
             $('#filelog').html(filetable);
         },
         fail: function(error) {
+            console.log("file log failed to load");
             console.log(error);
         }
     });
@@ -91,11 +93,13 @@ $(document).ready(function() {
     
                 success: function(data) {
                     console.log(data);
+                    console.log("file successfully uploaded"); //Error message for console
                     //Print success message
                     alert("Successfully submitted SVG");
                 },
                 error: function(error) {
                     //Print error message
+                    console.log("file failed to upload"); //Error message for console
                     alert("Invalid SVG or file");
                 }
             });
@@ -107,35 +111,214 @@ $(document).ready(function() {
         datatype: 'json',
         url: '/loadDropDown',
         success: function(filelog) {
-            var filedropdown = '<select id="images">';
+            console.log("dropdown successfully loaded"); //Error message for console debugging
+            var filedropdown = '<select id="images" class="svgdropdown">';
             //If returned array length is 0, disable dropdown list
             if (filelog.SVGLog.length == 0) {
                 console.log("empty");
                 filedropdown = '<select id="images" disabled>';
-                
+                filedropdown += '<option value="No files in server">No files in server</label>';
             } else { //Otherwise, run for loop that creates the dropdown,
                 console.log("there exists files");
                 //Set up all SVG options
                 console.log(filelog.SVGLog.length);
                 for (i = 0; i < filelog.SVGLog.length; i++) {
-                    filedropdown += '<option value="' + filelog.SVGLog[i] + '">' + filelog.SVGLog[i] + '</label>';
+                    if (i === 0) { //Ensure first element of dropdown is selected
+                        filedropdown += '<option selected value="' + filelog.SVGLog[i] + '">' + filelog.SVGLog[i] + '</label>';
+                    } else {
+                        filedropdown += '<option value="' + filelog.SVGLog[i] + '">' + filelog.SVGLog[i] + '</label>';
+                    }
                     //Use get??
                     console.log(i);
                 }
             }
             filedropdown += '</select>';
             $('#svgdropdown').html(filedropdown);
+            //Trigger the svg view to load
+            $('#svgdropdown').trigger("load", []);
         },
         error: function(error) {
+            console.log("dropdown failed to load"); //Error message for console debugging
             console.log(error);
         }
     });
+
+    //Loads svg table when dropdown loads
+    $('#svgdropdown').on("load", function() {
+        var detectFiles = null;
+        var selected = null;
+        var svgtable = "<table>";
+            var svgHeader = ["Title", "Description", "Component", "Summary", "Other Attributes"];
+
+        $.ajax({
+            type: "get",
+            datatype: 'json',
+            url: '/loadDropDown',
+            success: function(filelog) {
+                console.log("server scanned");
+                detectFiles = filelog.SVGLog.length;
+                if (detectFiles == 0) { //File is empty, do not ajax call, just set svgtable to empty
+                    //console.log("empty svg view");
+                    //Create empty base table
+                    svgtable += '<tr><th colspan="10" class="svgpanelimage"></th></tr>';
+            
+                    //Create first two headers
+                    svgtable += '<tr>';
+                    for (var i = 0; i < 2; i++) {
+                        svgtable +='<th colspan="3">' + svgHeader[i] + '</th>';
+                    }
+                    svgtable += '</tr>';
+    
+                    //Add empty cells for title and description
+                    svgtable += '<tr>';
+                    svgtable += '<td colspan="3"></td>';
+                    svgtable += '<td colspan="3"></td>';
+                    svgtable += '<tr>';
+    
+                    //Add last 3 headers
+                    svgtable += '<tr>';
+                    for (var i = 2; i < svgHeader.length; i++) {
+                        if (i == 3) {
+                            svgtable += '<th colspan="4">' + svgHeader[i] + '</th>';
+                        } else {
+                            svgtable += '<th>' + svgHeader[i] + '</th>';
+                        }
+                    }
+                    svgtable += '</tr>';
+
+                    //Create an empty cell
+                    svgtable += '<tr>';
+                    svgtable += '<td></td>';
+                    svgtable += '<td colspan="4"></td>';
+                    svgtable += '<td></td>';
+                    svgtable += '</tr>';
+
+                    svgtable += "</table>";
+                    //Update the SVG table 
+                    $('#svgview').html(svgtable);
+                } else { //File list is not empty
+                    //Set selected value and use ajax call to detect file appropriately
+                    selected = $('#images').children("option:selected").val();
+                    $.ajax({
+                        type: "get",
+                        datatype: 'json',
+                        data: {
+                            filename: selected,
+                        },
+                        url: '/loadViewSVG',
+                        success: function(fileStruct) {
+                            //console.log("SVG view successfully loaded"); //Error message for console
+                            //console.log(selected);
+                            //console.log(fileStruct.SVG.fileName);
+                            //console.log(fileStruct.SVG.titleDescObj);
+                            //console.log(fileStruct.SVG.titleDescObj.title);
+                            //console.log(fileStruct.SVG.titleDescObj.desc);
+            
+                            //Create picture
+                            svgtable += '<tr><th colspan="10"><img src="' + 'uploads/' + fileStruct.SVG.fileName + '" class="svgpanelimage"></th></tr>';
+            
+                            //Create first two headers + buttons
+                            svgtable += '<tr>';
+                            for (var i = 0; i < 2; i++) {
+                                svgtable +='<th colspan="3">' + svgHeader[i] +'</th>';
+                            }
+                            svgtable += '</tr>';
+            
+                            //Add cells for title and description
+                            svgtable += '<tr>';
+                            svgtable += '<td colspan="3">' + fileStruct.SVG.titleDescObj.title + '</td>';
+                            svgtable += '<td colspan="3">' + fileStruct.SVG.titleDescObj.desc + '</td>';
+                            svgtable += '<tr>';
+            
+                            //Add last 3 headers
+                            svgtable += '<tr>';
+                            for (var i = 2; i < svgHeader.length; i++) {
+                                if (i == 3) {
+                                    svgtable += '<th colspan="4">' + svgHeader[i] + '</th>';
+                                } else {
+                                    svgtable += '<th>' + svgHeader[i] + '</th>';
+                                }
+                            }
+                            svgtable += '</tr>';
+            
+                            //Add appropriate cells for the file given
+                            //start with rectangles, circles, paths, then group
+                            for (var i = 0; i < fileStruct.SVG.rectListObj.length; i++) {
+                                svgtable += '<tr>';
+                                svgtable += '<td>Rectangle ' + (i+1) + '</td>';
+                                if (fileStruct.SVG.rectListObj[i].units != "") {
+                                    svgtable += '<td colspan="4">Upper left corner: x = ' + fileStruct.SVG.rectListObj[i].x + fileStruct.SVG.rectListObj[i].units;
+                                    svgtable += ' y = ' + fileStruct.SVG.rectListObj[i].y + fileStruct.SVG.rectListObj[i].units;
+                                    svgtable += '<br>Width = ' + fileStruct.SVG.rectListObj[i].w + fileStruct.SVG.rectListObj[i].units;
+                                    svgtable += ' Height = ' + fileStruct.SVG.rectListObj[i].h + fileStruct.SVG.rectListObj[i].units;
+                                    svgtable += '</td>';
+                                } else {
+                                    svgtable += '<td colspan="4">Upper left corner: x = ' + fileStruct.SVG.rectListObj[i].x;
+                                    svgtable += ' y = ' + fileStruct.SVG.rectListObj[i].y;
+                                    svgtable += '<br>Width = ' + fileStruct.SVG.rectListObj[i].w;
+                                    svgtable += ' Height = ' + fileStruct.SVG.rectListObj[i].h;
+                                    svgtable += '</td>';
+                                }
+                                svgtable += '<td>' + fileStruct.SVG.rectListObj[i].numAttr + '</td>';
+                                svgtable += '</tr>';
+                            } //Add all circles
+                            for (var i = 0; i < fileStruct.SVG.circListObj.length; i++) {
+                                svgtable += '<tr>';
+                                svgtable += '<td>Circle ' + (i+1) + '</td>';
+                                //Check if units is empty
+                                if (fileStruct.SVG.circListObj[i].units != "") {
+                                    svgtable += '<td colspan="4">Centre: x = ' + fileStruct.SVG.circListObj[i].cx + fileStruct.SVG.circListObj[i].units;
+                                    svgtable += ' y = ' + fileStruct.SVG.circListObj[i].cy + fileStruct.SVG.circListObj[i].units;
+                                    svgtable += ' radius = ' + fileStruct.SVG.circListObj[i].r + fileStruct.SVG.circListObj[i].units;
+                                    svgtable += '</td>';
+                                } else {
+                                    svgtable += '<td colspan="4">Centre: x = ' + fileStruct.SVG.circListObj[i].cx;
+                                    svgtable += ' y = ' + fileStruct.SVG.circListObj[i].cy;
+                                    svgtable += ' radius = ' + fileStruct.SVG.circListObj[i].r;
+                                    svgtable += '</td>';
+                                }
+                                svgtable += '<td>' + fileStruct.SVG.circListObj[i].numAttr + '</td>';
+                                svgtable += '</tr>';
+                            } //Add all paths
+                            for (var i = 0; i < fileStruct.SVG.pathListObj.length; i++) {
+                                svgtable += '<tr>';
+                                svgtable += '<td>Path ' + (i+1) + '</td>';
+                                svgtable += '<td colspan="4">path data = ' + fileStruct.SVG.pathListObj[i].d + '</td>';
+                                svgtable += '<td>' + fileStruct.SVG.pathListObj[i].numAttr + '</td>';
+                                svgtable += '</tr>';
+                            } //Add all groups
+                            for (var i = 0; i < fileStruct.SVG.groupListObj.length; i++) {
+                                svgtable += '<tr>';
+                                svgtable += '<td>Group ' + (i+1) + '</td>';
+                                svgtable += '<td colspan="4">' + fileStruct.SVG.groupListObj[i].children + ' child elements</td>';
+                                svgtable += '<td>' + fileStruct.SVG.groupListObj[i].numAttr + '</td>';
+                                svgtable += '</tr>';
+                            }
+                            svgtable += "</table>";
+                            //Update the SVG table 
+                            $('#svgview').html(svgtable);
+                        },
+                        error: function(error) {
+                            console.log("SVG view failed to load"); //Error message for debugging
+                            console.log(error);
+                        }
+                    });
+                }
+            },
+            error: function(error) {
+                console.log("no files received"); //Error message for console debugging
+                console.log(error);
+            }
+        });
+    });
+        //console.log(selected);
 
     //When dropdown changes, adjust SVG view panel
     $('#svgdropdown').change(function() {
         var selected = $('#images').children("option:selected").val();
         var svgtable = "<table>";
             var svgHeader = ["Title", "Description", "Component", "Summary", "Other Attributes"];
+
         //console.log(selected);
         //Ajax call that loads the SVG View Panel
         $.ajax({
@@ -146,11 +329,12 @@ $(document).ready(function() {
             },
             url: '/loadViewSVG',
             success: function(fileStruct) {
-                console.log(selected);
-                console.log(fileStruct.SVG.fileName);
-                console.log(fileStruct.SVG.titleDescObj);
-                console.log(fileStruct.SVG.titleDescObj.title);
-                console.log(fileStruct.SVG.titleDescObj.desc);
+                console.log("SVG view successfully loaded"); //Error message for console
+                //console.log(selected);
+                //console.log(fileStruct.SVG.fileName);
+                //console.log(fileStruct.SVG.titleDescObj);
+                //console.log(fileStruct.SVG.titleDescObj.title);
+                //console.log(fileStruct.SVG.titleDescObj.desc);
 
                 //Create picture
                 svgtable += '<tr><th colspan="10"><img src="' + 'uploads/' + fileStruct.SVG.fileName + '" class="svgpanelimage"></th></tr>';
@@ -237,10 +421,37 @@ $(document).ready(function() {
                 $('#svgview').html(svgtable);
             },
             error: function(error) {
+                console.log("SVG view failed to load"); //Error message for debugging
                 console.log(error);
             }
         });
     });
+
+    //Event handles an edit title press
+    $('#editTitle').on("click", function(e) {
+        console.log("Edit title");
+    });
+
+    //Event handles an edit description press
+    $('#editDesc').on("click", function(e) {
+        console.log("Edit description");
+    });
+
+    //Event handles a create SVG press
+    $('#submitNewSVG').on("click", function(e) {
+        console.log("Create new SVG");
+    });
+
+    //Event handles a create component press
+    $('#submitComponent').on("click", function(e) {
+        console.log("Create new component");
+    });
+
+    //Event handles a create shape press
+    $('#reshapeComponents').on("click", function(e) {
+        console.log("Reshape new component");
+    });
+
 
     // Event listener form example , we can use this instead explicitly listening for events
     // No redirects if possible
